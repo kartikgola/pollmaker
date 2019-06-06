9a699f45fae0e4a5cf09e021c0a21bab2e3f80a4
Kartik Gola <krtkgla@gmail.com>
2019-06-06 18:03:14 +0530

    First commit!

---
+++ src/app/polls/polls.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PollsRoutingModule } from './polls-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PollsRoutingModule
+  ]
+})
+export class PollsModule { }
