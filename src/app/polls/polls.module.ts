29ed9fcba99496b07e57844209f6842245e8ba71
ritugarg99 <ritugarg92010@gmail.com>
2019-06-08 15:18:38 +0200

    adding polllist comp dec

--- src/app/polls/polls.module.ts
+++ src/app/polls/polls.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { PollListComponent } from './poll-list/poll-list.component';
 import { PollsRoutingModule } from './polls-routing.module';
 import { PollDetailComponent } from './poll-detail/poll-detail.component';
 
 @NgModule({
-  declarations: [PollDetailComponent],
+  declarations: [
+    PollDetailComponent, 
+    PollListComponent
+  ],
   imports: [
     CommonModule,
     PollsRoutingModule
